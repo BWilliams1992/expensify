aa677894a38521d2452319f75529ee7851af3062
Ben Williams <bwilliams0001992@gmail.com>
2020-05-15 22:10:47 +0100

    implemented user auth

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,6 @@
+require('dotenv').config({ path: '.env.test' });
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-require('dotenv').config({ path: '.env.test'})
 
 Enzyme.configure({
   adapter: new Adapter()
